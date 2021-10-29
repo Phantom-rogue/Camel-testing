10d558c928e9a67a0596859560324d1e63dce07e
Phantom-rogue <69292536+Phantom-rogue@users.noreply.github.com>
2021-10-29 12:07:12 +0530

    added camel

---
+++ camel-a/camel-a/src/main/java/com/cg/apchesample/camela/CamelAApplication.java
@@ -0,0 +1,13 @@
+package com.cg.apchesample.camela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelAApplication.class, args);
+	}
+
+}
